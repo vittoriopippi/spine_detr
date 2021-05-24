09efa0df174af253e9c7780d8965860169380f92
vittoriopippi <219424@studenti.unimore.it>
2021-05-24 10:15:40 +0200

    Fix

--- launch_docker.sh
+++ launch_docker.sh
@@ -1,1 +1,3 @@
-docker run -v "/home/data/Diagnostikbilanz/Fertig 20190503":"/home/data/Diagnostikbilanz/Fertig 20190503" -v /home/vpippi/projects/ostfalia_detr/spine_plot:/workspace/spine_plot -v /home/vpippi/projects/ostfalia_detr/logs:/workspace/logs -u $(whoami) --gpus all -it spine-detr
+export UID=$(id -u)
+export GID=$(id -g)
+docker run -v "/home/data/Diagnostikbilanz/Fertig 20190503":"/home/data/Diagnostikbilanz/Fertig 20190503" -v /home/vpippi/projects/ostfalia_detr/spine_plot:/workspace/spine_plot -v /home/vpippi/projects/ostfalia_detr/logs:/workspace/logs --user $UID:$GID --gpus all -it spine-detr
