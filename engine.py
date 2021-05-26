fde17a8299eab7d548e0b43b69340280f448faf6
vittoriopippi <219424@studenti.unimore.it>
2021-05-26 14:26:10 +0200

    Bug spine evaluation

--- engine.py
+++ engine.py
@@ -38,7 +38,7 @@ def plot_images(writer, step, samples, outputs, targets, indices, epoch, i, tag=
         img.save(f'{folder}/{epoch:03d}_{i:03d}_{d:02d}_{tag}_out.jpg')
         # writer.add_image(f'{tag}/out_points', F.to_tensor(img), global_step=step + d)
 
-def spine_evaluation(src_outputs, logits, src_targets, spacing, threshold=0.5, r=10):
+def spine_evaluation(src_outputs, logits, src_targets, spacing, args, threshold=0.5, r=10):
     outputs, targets = torch.clone(src_outputs).detach(), torch.clone(src_targets).detach()
     outputs = outputs[logits.squeeze(-1) > threshold]
     C = torch.cdist(outputs, targets, p=2).cpu()
@@ -46,7 +46,7 @@ def spine_evaluation(src_outputs, logits, src_targets, spacing, threshold=0.5, r
     outputs, targets = outputs[idx_out], targets[idx_tar]
     dist = torch.cdist(outputs, targets, p=2).diag()
 
-    dist *= 360 # which is the random crop size
+    dist *= args.rand_crop # which is the random crop size
     dist *= spacing # actual distance in mm
 
     in_dist = dist[dist < r]
@@ -93,7 +93,7 @@ def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
             plot_images(writer, step, samples, outputs, targets, indices, epoch, i, tag='train', folder=args.comment)
 
         for i in range(len(samples)):
-            FN, FP, TP, in_dist = spine_evaluation(outputs['pred_boxes'][i], outputs['pred_logits'][i], targets[i][:, 1:3], info[i]['spacing'])
+            FN, FP, TP, in_dist = spine_evaluation(outputs['pred_boxes'][i], outputs['pred_logits'][i], targets[i][:, 1:3], info[i]['spacing'], args)
             FNs.append(FN)
             FPs.append(FP)
             TPs.append(TP)
@@ -176,7 +176,7 @@ def evaluate(model, criterion, postprocessors, data_loader, base_ds, device, out
             plot_images(writer, step, samples, outputs, targets, indices, epoch, i, tag='test', folder=args.comment)
 
         for i in range(len(samples)):
-            FN, FP, TP, in_dist = spine_evaluation(outputs['pred_boxes'][i], outputs['pred_logits'][i], targets[i][:, 1:3], info[i]['spacing'])
+            FN, FP, TP, in_dist = spine_evaluation(outputs['pred_boxes'][i], outputs['pred_logits'][i], targets[i][:, 1:3], info[i]['spacing'], args)
             FNs.append(FN)
             FPs.append(FP)
             TPs.append(TP)
