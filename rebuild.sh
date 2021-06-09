2c8fe060a836a5b9ff7b1009174dac11cb8b03f9
vittoriopippi <219424@studenti.unimore.it>
2021-06-09 10:47:30 +0200

    .

--- rebuild.sh
+++ rebuild.sh
@@ -1,2 +1,2 @@
 git pull
-docker build --build-arg USER_ID=$UID -t spine-detr . && ./launch_docker.sh
+docker build --build-arg USER_ID=$UID --build-arg USER_ID=$(id -g) -t spine-detr . && ./launch_docker.sh
